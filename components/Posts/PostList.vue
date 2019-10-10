734ce6501202a7a7a45800ccae9dff720dce8421
Duy Nguyen <andy@tier3interactive.com>
2019-10-10 16:17:04 -0400

    Initial commit

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,55 @@
+<template>
+  <section class="posts-list">
+    <PostPreview 
+      id="1" 
+      :is-admin="isAdmin"
+      thumbnail="https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg" 
+      title="Post Title" 
+      preview-text="Post Content" 
+    />
+
+    <PostPreview 
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg"
+      title="Post Title"
+      preview-text="Post Content"
+    />
+
+    <PostPreview 
+      id="1"
+      :is-admin="isAdmin"
+      thumbnail="https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg"
+      title="Post Title"
+      preview-text="Post Content"
+    />
+  </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview'
+
+export default {
+  components: {
+    PostPreview
+  },
+
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+.posts-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="admin-post-page">
+    <section class="update-form">
+      <AdminPostForm :post="loadedPost" />
+    </section>
+  </div>
+</template>
+
+<script>
+import AdminPostForm from '@/components/Admin/AdminPostForm'
+
+export default {
+  layout: 'admin',
+  components: {
+    AdminPostForm
+  },
+
+  data() {
+    return {
+      loadedPost: {
+        author: 'Andy Nguyen',
+        title: 'This Post',
+        content: 'Super Amazing content.',
+        thumbnailLink: 'https://images.pexels.com/photos/373543/pexels-photo-373543.jpeg'
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.update-form {
+  width: 90%;
+  margin: 20px auto;
+}
+
+@media (min-width: 768px) {
+  .update-form {
+    width: 500px;
+  }
+}
+</style>
